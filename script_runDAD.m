341098ec0f28f71ef92cf92e9d4a931d6b4751a2
Eva Dyer <evaLdyer@gmail.com>
2017-10-21 19:27:13 -0400

    runDAD script

--- script_runDAD.m
+++ script_runDAD.m
@@ -1,66 +1,28 @@
-%script_run3D_DAD_Mihi
+%% 1. Prepare data and compute firing rates
 
-removedir = [0,1,2,6];
-Data = prepare_superviseddata(0.22,'chewie1','mihi',[],0);
+% bin size = 200 ms, chewie1 for training kinematics, mihi for neural test
+Data = prepare_superviseddata(0.2,'chewie1','mihi',[],0); 
+
+% subsample the kinematics and neural data
+removedir = [0,1,2,6]; 
 [Xte,Yte,Tte,Xtr,Ytr,Ttr,Nte,Ntr] = removedirdata(Data,removedir);
-Yte2 = downsamp_nd(Yte,0);
 
-%%
-opts.dimred_method = 'FA';
-[Xrec1,~,~,~,tmp] = runDAD3d(Xtr,Yte,opts);
-R2valDAD(1) = evalR2(Xrec1,Xte);
-PcorrDAD(1) = evalTargetErr(Xrec1,Xte,Tte);
-minKLDAD(1) = min(tmp(:));
+disp('Finished computing firing rates...')
 
-%%
-opts.dimred_method = 'PCA';
-[Xrec2,~,~,~,tmp] = runDAD3d(Xtr,Yte,opts);
-R2valDAD(2) = evalR2(Xrec2,Xte);
-PcorrDAD(2) = evalTargetErr(Xrec2,Xte,Tte);
-minKLDAD(2) = min(tmp(:));
+%% 2. Run DAD w/ factor analysis for dimensionality reduction
+% opts.dimred_method = 'PCA'; % to apply PCA
 
-%%
-opts.dimred_method = 'Isomap';
-[Xrec3,~,~,~,tmp] = runDAD3d(Xtr,Yte,opts);
-R2valDAD(3) = evalR2(Xrec3,Xte);
-PcorrDAD(3) = evalTargetErr(Xrec3,Xte,Tte);
-minKLDAD(3) = min(tmp(:));
-
-%%
-%%%% Supervised methods (run oracle and kalman filter)
-Xr = LSoracle(Xte,Yte);
-R2Oracle = evalR2(Xr,Xte);  
-PcorrOracle = evalTargetErr(Xr,Xte,Tte);
-
-[~,~,tmp,~] = rotated_KLmin(Xr,Xtr,90);
-minKLOracle = min(tmp(:));
-
-[W, Xsup, R2Max, lamc]= crossVaL(Yte, Xte, 500, 100);
-R2Sup = evalR2(Xsup,Xte); 
-PcorrSup = evalTargetErr(Xsup,Xte,Tte);
-
-[~,~,tmp,~] = rotated_KLmin(Xsup,Xtr,90);
-minKLSup = min(tmp(:));
-
+opts.dimred_method = 'FA'; % use factor analysis to reduce the dimensionality
+Xrec = runDAD3d(Xtr,Yte,opts);
+R2valDAD = evalR2(Xrec,Xte); % compute the R2
+disp('Finished running DAD...')
 
 %%
 
 figure,
-subplot(1,6,1),
+subplot(1,3,1),
 colorData(Xtr,Ttr), title('Training')
-subplot(1,6,2),
+subplot(1,3,2),
 colorData(Xte,Tte), title('Test')
-subplot(1,6,3),
-colorData(Xsup,Tte), title(['Supervised (R2 = ', num2str(R2Sup,2), ')'])
-subplot(1,6,4),
-colorData(Xrec1,Tte), title(['DAD (R2 = ', num2str(R2valDAD(1),2), ')'])
-subplot(1,6,5),
-colorData(Xrec2,Tte), title(['DAD (R2 = ', num2str(R2valDAD(2),2), ')'])
-subplot(1,6,6),
-colorData(Xrec3,Tte), title(['DAD (R2 = ', num2str(R2valDAD(3),2), ')'])
-
-
-%%
-figure, 
-bar([[R2Oracle, R2Sup, R2valDAD]; [PcorrOracle, PcorrSup, PcorrDAD]; ...
-    [minKLOracle, minKLSup, minKLDAD]]' )
+subplot(1,3,3),
+colorData(Xrec,Tte), title(['DAD (R2 = ', num2str(R2valDAD,2), ')'])
