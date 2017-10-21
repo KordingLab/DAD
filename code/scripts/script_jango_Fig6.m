4535b1f75b7a14d5c8834f905827366460e64fcf
Eva Dyer <evaLdyer@gmail.com>
2017-10-21 19:36:02 -0400

    jango_Fig6 script
    added comments

--- code/scripts/script_jango_Fig6.m
+++ code/scripts/script_jango_Fig6.m
@@ -13,7 +13,7 @@ opts.sfac = 4;
 opts.check2D = 1;
 
 %%
-% download data
+% compile data from Subject J
 [Yte1,Tte1,Xte1] = compile_jango_neuraldata(whichtarg,opts.numS,opts.numD,'binnedData_0801.mat');
 Yte1 = downsamp_nd(Yte1,opts.sfac);
 [Yte2,Tte2,Xte2] = compile_jango_neuraldata(whichtarg,opts.numS,opts.numD,'binnedData_0807.mat');
@@ -23,28 +23,58 @@ Yte3 = downsamp_nd(Yte3,opts.sfac);
 [Yte4,Tte4,Xte4] = compile_jango_neuraldata(whichtarg,opts.numS,opts.numD,'binnedData_0901.mat');
 Yte4 = downsamp_nd(Yte4,opts.sfac);
 
-%%
+%% Run DAD on data from four time points
+
+% neural data J1, kinematics test J1
 [Vout11] = runDAD2d(Yte1,Xte1,opts.dimred_method);
+
+% neural data J2, kinematics test J1
 [Vout12] = runDAD2d(Yte2,Xte1,opts.dimred_method);
+
+% neural data J3, kinematics test J1
 [Vout13] = runDAD2d(Yte3,Xte1,opts.dimred_method);
+
+% neural data J4, kinematics test J1
 [Vout14] = runDAD2d(Yte4,Xte1,opts.dimred_method);
 
+% neural data J1, kinematics test J2
 [Vout21] = runDAD2d(Yte1,Xte2,opts.dimred_method);
+
+% neural data J2, kinematics test J2
 [Vout22] = runDAD2d(Yte2,Xte2,opts.dimred_method);
+
+% neural data J3, kinematics test J2
 [Vout23] = runDAD2d(Yte3,Xte2,opts.dimred_method);
+
+% neural data J4, kinematics test J2
 [Vout24] = runDAD2d(Yte4,Xte2,opts.dimred_method);
 
+% neural data J1, kinematics test J3
 [Vout31] = runDAD2d(Yte1,Xte3,opts.dimred_method);
+
+% neural data J2, kinematics test J3
 [Vout32] = runDAD2d(Yte2,Xte3,opts.dimred_method);
+
+% neural data J3, kinematics test J3
 [Vout33] = runDAD2d(Yte3,Xte3,opts.dimred_method);
+
+% neural data J4, kinematics test J3
 [Vout34] = runDAD2d(Yte4,Xte3,opts.dimred_method);
 
+% neural data J1, kinematics test J4
 [Vout41] = runDAD2d(Yte1,Xte4,opts.dimred_method);
+
+% neural data J2, kinematics test J4
 [Vout42] = runDAD2d(Yte2,Xte4,opts.dimred_method);
+
+% neural data J3, kinematics test J4
 [Vout43] = runDAD2d(Yte3,Xte4,opts.dimred_method);
+
+% neural data J4, kinematics test J4
 [Vout44] = runDAD2d(Yte4,Xte4,opts.dimred_method);
 
-%%
+%% Visualize results
+
 figure,
 subplot(4,4,1), colorData(Vout11,Tte1), title(num2str(evalR2(Vout11,Xte1(:,1:2)),3))
 subplot(4,4,2), colorData(Vout12,Tte2), title(num2str(evalR2(Vout12,Xte2(:,1:2)),3))
@@ -65,12 +95,6 @@ subplot(4,4,13), colorData(Vout41,Tte1), title(num2str(evalR2(Vout41,Xte1(:,1:2)
 subplot(4,4,14), colorData(Vout42,Tte2), title(num2str(evalR2(Vout42,Xte2(:,1:2)),3))
 subplot(4,4,15), colorData(Vout43,Tte3), title(num2str(evalR2(Vout43,Xte3(:,1:2)),3))
 subplot(4,4,16), colorData(Vout44,Tte4), title(num2str(evalR2(Vout44,Xte4(:,1:2)),3))
-
-%[Xrec2,Vflip2,Vr2] = runDAD3d(Xtr,Yte2,opts);
-%[Xrec3,Vflip3,Vr3] = runDAD3d(Xtr,Yte3,opts);
-%[Xrec4,Vflip4,Vr4] = runDAD3d(Xtr,Yte4,opts);
-
-%Hinv = Xrec2(:,1:2)'*pinv(Yte2)';
 
 
 
