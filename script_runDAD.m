0f2d19658eda5181915da8728240333470b39eba
Eva Dyer <evaLdyer@gmail.com>
2017-12-22 12:17:16 -0500

    updated script to use demo data folder (rather than downloading data)

--- script_runDAD.m
+++ script_runDAD.m
@@ -1,16 +1,16 @@
-%% 1. Prepare data and compute firing rates
+%% 0. Prepare data and compute firing rates
 
 % bin size = 200 ms, chewie1 for training kinematics, mihi for neural test
-Data = prepare_superviseddata(0.2,'chewie1','mihi',[],0); 
+%Data = prepare_superviseddata(0.2,'chewie1','mihi',[],0);
 
 % subsample the kinematics and neural data
-removedir = [0,1,2,6]; 
-[Xte,Yte,Tte,Xtr,Ytr,Ttr,Nte,Ntr] = removedirdata(Data,removedir);
+%removedir = [0,1,2,6];
+%[Xte,Yte,Tte,Xtr,Ytr,Ttr,Nte,Ntr] = removedirdata(Data,removedir);
 
-disp('Finished computing firing rates...')
+%disp('Finished computing firing rates...')
 
-%% 2. Run DAD w/ factor analysis for dimensionality reduction
-% opts.dimred_method = 'PCA'; % to apply PCA
+%% 1. Run DAD w/ factor analysis for dimensionality reduction
+% opts.dimred_method = 'PCA'; % to apply PCA instead of FA
 
 opts.dimred_method = 'FA'; % use factor analysis to reduce the dimensionality
 Xrec = runDAD3d(Xtr,Yte,opts);
